62fe637983f0fae49a02b845bb47f6c7be73a67c
IbrahimElhayani0023 <ibrahimelh23@gmail.com>
2025-02-25 23:47:41 +0000

    connect to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./db/connectDb.js";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World 123");
 });
 
 
 app.listen(5000, () => {
+    connectDb();
     console.log("Server is running on port 5000");
 });
